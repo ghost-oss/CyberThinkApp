19ea21a98258c5624267e72ba9fd0c04c12664c0
Sahil_Ramanuj <sahil_ramanuj@next.co.uk>
2022-01-28 21:00:31 +0000

    Implementation for the following:
    CreateNote_ViewController:
    - Regex and empty string validation have been implemented

--- ViewControllers/CreateNote_ViewController.cs
+++ ViewControllers/CreateNote_ViewController.cs
@@ -1,7 +1,7 @@
 using System;
 using CyberThink.Helpers;
 using CyberThink.ViewModel;
-using Foundation;
+using System.Text.RegularExpressions;
 using UIKit;
 
 namespace CyberThink
@@ -65,14 +65,64 @@ namespace CyberThink
         public void SaveNoteBtnClicked(object sender, EventArgs e)
         {
             string noteTitle = noteTitleTextField.Text;
-            var note = noteTextView.Text;
-            createNotesViewModel.CreateNote(noteTitle,note);
+            string note = noteTextView.Text;
+
+            //Invalid Char input
+            note = RegExValidation(note);
+            noteTitle = RegExValidation(noteTitle);
+
+            //Text input 
+            if (!ValidateTextInput(noteTitle,note))
+            {
+                var alert = UIAlertController.Create("Error", "Both fields must be filled", UIAlertControllerStyle.Alert);
+                var action = UIAlertAction.Create("Ok", UIAlertActionStyle.Destructive, (obj) => { });
+                alert.AddAction(action);
+                this.PresentViewController(alert, true, null);
+            }
+            else
+            { 
+                createNotesViewModel.CreateNote(noteTitle, note);
+            }
 
             //Goes back to notes list 
             this.NavigationController.PopToRootViewController(true);
 
         }
-    
+
+
+        public bool ValidateTextInput(string noteTitle, string note)
+        {
+
+            if ((string.IsNullOrEmpty(noteTitle) || string.IsNullOrEmpty(note)))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+           
+        }
+
+        public string RegExValidation(string input)
+        {
+            //var regex = new Regex("[^a-zA-Z0-9 _.,!()]");
+            //regex.Replace(input, string.Empty);
+
+            var sanitizedStr = Regex.Replace(input, @"[^\w\d\.@!%*&$+='{}\()\[\].?: -]", "");
+
+            if (string.IsNullOrEmpty(sanitizedStr) && !string.Equals(sanitizedStr, input))
+            {
+                var alert = UIAlertController.Create("Error", "Invalid characters detected", UIAlertControllerStyle.Alert);
+                var action = UIAlertAction.Create("Ok", UIAlertActionStyle.Destructive, (obj) => { });
+                alert.AddAction(action);
+                this.PresentViewController(alert, true, null);
+            }
+
+            return sanitizedStr;
+        }
+
+
         
     }
 }
